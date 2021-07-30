9c689f8e3a285ff554bd00cfec16bdcfa77ea4bb
taigger <hidenavi92soh1390@gmail.com>
2021-07-30 13:28:20 +0000

    latesttt

--- resources/views/teacher/auth/register.blade.php
+++ resources/views/teacher/auth/register.blade.php
@@ -145,13 +145,13 @@
                         <!--    <textarea class="form-control" name="introsuction" id="exampleFormControlTextarea1" rows="10"></textarea>-->
                         <!--    </div>-->
 
-                        <!--    <div class="form-group row mb-0">-->
-                        <!--        <div class="col-md-6 offset-md-4">-->
-                        <!--        <button type="submit" class="btn btn-primary">-->
-                        <!--            {{ __('Register') }}-->
-                        <!--        </button>-->
-                        <!--        </div>-->
-                        <!--    </div>-->
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                                </div>
+                            </div>
                    
                         <!--</div>-->
                     </form>
